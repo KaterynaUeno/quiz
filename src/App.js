8c9990b2a1ad0bedda7e86d42aee50029c5f009b
Kateryna Ueno <katerynaueno@gmail.com>
2021-12-07 16:13:38 +0000

    setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Start from "./components/Start";
+import Qestion from "./components/Question";
 const App = () => {
   return (
     <div className="App">
-      <Start />
+      {/* <Start /> */}
+      <Qestion />
     </div>
   );
 };
---
+++ src/components/Question.js
@@ -0,0 +1,33 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./styleStart.css";
+
+const Question = () => {
+  const nextClick = (e) => {};
+  return (
+    <div className="card">
+      <div className="main">
+        <div className="content">
+          <h2>Question</h2>
+          <label className="label">
+            <input type="radio" name="answer"></input>
+            Choice
+          </label>
+          <label className="label">
+            <input type="radio" name="answer"></input>
+            Choice
+          </label>
+          <label className="label">
+            <input type="radio" name="answer"></input>
+            Choice
+          </label>
+        </div>
+        <div className="error"> error</div>
+        <button className="btn" onClick={() => nextClick}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Question;
